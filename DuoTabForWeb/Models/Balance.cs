26a53a281c648bd2cc3965f536f80456231cc1d6
Undrea <isabellahugel@gmail.com>
2016-10-24 22:48:54 -0600

    Added Balance model

---
+++ DuoTabForWeb/Models/Balance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuoTabForWeb.Models
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public int BalanceInCents { get; set; }
+
+        /// <summary>
+        /// Initializes the Model with a starting balance in cents (e.g. 125 cents).
+        /// </summary>
+        /// <param name="balance">Pass the value in cents; a negative value means "partner" owes "you".</param>
+        public Balance(int balanceInCents)
+        {
+            BalanceInCents = balanceInCents;
+        }
+
+        /// <summary>
+        /// Initializes the Model with a starting currency balance (e.g. 12.55 dollars).
+        /// </summary>
+        /// <param name="balanceInCurrency"></param>
+        public Balance(float balanceInCurrency)
+        {
+            // Convert the value to an integer and round to the nearest cent
+            float valueInCents = balanceInCurrency * 100;
+            BalanceInCents = Convert.ToInt32(Math.Round(valueInCents));
+        }
+    }
+}
