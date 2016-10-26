6a8166604c934d96b880bf7c3bef2783e1117537
Undrea <isabellahugel@gmail.com>
2016-10-25 23:58:15 -0600

    Added ExpenseTypes enumeration

---
+++ DuoTabForWeb/Models/ExpenseType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuoTabForWeb.Enums
+{
+    public enum ExpenseType
+    {
+        Auto,
+        Bills,
+        Coffee,
+        Charity,
+        Childcare,
+        Debts,
+        Dinner,
+        Drinks,
+        Education,
+        Entertainment,
+        Fuel,
+        Food,
+        Gifts,
+        Hobbies,
+        Household,
+        Housing,
+        Insurance,
+        Miscellaneous,
+        Pets,
+        Prescriptions,
+        Subscriptions,
+        Transportation,
+        Utilities,
+        Vacation
+    }
+}
