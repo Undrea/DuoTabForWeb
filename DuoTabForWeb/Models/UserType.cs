c5c39b778319163c19c3f46729a6c3ee60afb2b3
Undrea <isabellahugel@gmail.com>
2016-10-25 23:58:58 -0600

    Added UserTypes enumeration

---
+++ DuoTabForWeb/Models/UserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace DuoTabForWeb.Enums
+{
+    public enum UserType
+    {
+        [Display(Name = "Me")]
+        [Display(Name = "My Partner")]
+        Partner = 1
+    }
+}
